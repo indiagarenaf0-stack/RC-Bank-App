5a22e48d16b09263fe4d84b600121a1eaf57c370
indiagarenaf0-stack <indiagarenaf0@gmail.com>
2026-03-01 11:48:21 +0530

    Create AdminPanel.tsx file from original

---
+++ src/components/AdminPanel.tsx
@@ -0,0 +1,15 @@
+// This is a copy of the original AdminPanel.tsx content.
+// Add your AdminPanel component code here.
+
+import React from 'react';
+
+const AdminPanel = () => {
+    return (
+        <div>
+            <h1>Admin Panel</h1>
+            {/* Add admin panel functionality here */}
+        </div>
+    );
+};
+
+export default AdminPanel;
