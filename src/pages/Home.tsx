d5748c3ba2c7bcdb9a10be073c1e205f491d21ed
indiagarenaf0-stack <indiagarenaf0@gmail.com>
2026-03-01 11:44:41 +0530

    Create Home.tsx file in src/pages directory

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+// This is the content of the original Home.tsx file 
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the Home Page</h1>
+        </div>
+    );
+};
+
+export default Home;
