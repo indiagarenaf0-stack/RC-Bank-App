970cf96e801042776c8a943cce33595d0495ebd5
indiagarenaf0-stack <indiagarenaf0@gmail.com>
2026-03-01 11:47:28 +0530

    Create AuthCallback.tsx file

---
+++ src/pages/AuthCallback.tsx
@@ -0,0 +1,13 @@
+// Original content of AuthCallback.tsx should be placed here.
+// Example structure:
+import React from 'react';
+
+const AuthCallback = () => {
+    return (
+        <div>
+            Auth Callback Component
+        </div>
+    );
+};
+
+export default AuthCallback;
