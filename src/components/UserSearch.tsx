58c312e86c620a869aeb4e726bcbdfb8448002f5
indiagarenaf0-stack <indiagarenaf0@gmail.com>
2026-03-01 11:49:59 +0530

    Create UserSearch component file

---
+++ src/components/UserSearch.tsx
@@ -0,0 +1,13 @@
+// This is a new UserSearch Component
+import React from 'react';
+
+const UserSearch = () => {
+    return (
+        <div>
+            <h1>User Search</h1>
+            {/* Implement search functionality here */}
+        </div>
+    );
+};
+
+export default UserSearch;
