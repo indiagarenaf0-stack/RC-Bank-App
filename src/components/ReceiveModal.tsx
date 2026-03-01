42cdb79f24ccb1412196a35f7ba297fb058f07eb
indiagarenaf0-stack <indiagarenaf0@gmail.com>
2026-03-01 11:49:20 +0530

    Create ReceiveModal component file

---
+++ src/components/ReceiveModal.tsx
@@ -0,0 +1,13 @@
+// This is the new ReceiveModal component
+
+import React from 'react';
+
+const ReceiveModal = () => {
+    return (
+        <div>
+            <h1>Receive Modal</h1>
+        </div>
+    );
+};
+
+export default ReceiveModal;
